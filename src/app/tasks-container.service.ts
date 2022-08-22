1c88391d068d09f7ec2dc5e2f4d63c782e5bc254
Stasiuk.Illia <Stasiuk.Illia@scheidt-bachmann.de>
2022-08-22 18:06:20 +0300

    Cosmetical changes in TaskContainerService

--- src/app/tasks-container.service.ts
+++ src/app/tasks-container.service.ts
@@ -3,10 +3,9 @@ import { BehaviorSubject } from 'rxjs';
 import { Task, TaskStatus } from './task.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksContainerService {
-
   private pendingTasksSource = new BehaviorSubject<Task[]>([]);
   private doneTasksSource = new BehaviorSubject<Task[]>([]);
   pendingTasks = this.pendingTasksSource.asObservable();
@@ -16,59 +15,67 @@ export class TasksContainerService {
 
   addPendingTask(id: number, description: string, due?: Date) {
     const toEmit = this.pendingTasksSource.getValue();
-    toEmit.push(
-      {
-        id: id,
-        description: description,
-        dueDate: due,
-        doneDate: undefined,
-        status: TaskStatus.Pending
-      }
-      );
+    toEmit.push({
+      id: id,
+      description: description,
+      dueDate: due,
+      doneDate: undefined,
+      status: TaskStatus.Pending,
+    });
     this.pendingTasksSource.next(toEmit);
   }
 
   markTaskDone(id: number) {
-    const doneTask = this.pendingTasksSource.getValue().find(
-      task => task.id === id
-    );
+    const doneTask = this.pendingTasksSource
+      .getValue()
+      .find((task) => task.id === id);
 
     if (doneTask) {
-      const toEmitPending = this.pendingTasksSource.getValue().filter(task => task !== doneTask);
+      const toEmitPending = this.pendingTasksSource
+        .getValue()
+        .filter((task) => task !== doneTask);
       const toEmitDone = this.doneTasksSource.getValue();
       doneTask.status = TaskStatus.Done;
       doneTask.doneDate = new Date();
       toEmitDone.push(doneTask);
       this.pendingTasksSource.next(toEmitPending);
-      this.doneTasksSource.next(toEmitDone);   
+      this.doneTasksSource.next(toEmitDone);
     }
   }
 
   markTaskPending(id: number) {
-    const pendingTask = this.doneTasksSource.getValue().find(
-      task => task.id === id
-    );
+    const pendingTask = this.doneTasksSource
+      .getValue()
+      .find((task) => task.id === id);
 
     if (pendingTask) {
-      const toEmitDone = this.doneTasksSource.getValue().filter(task => task !== pendingTask);
+      const toEmitDone = this.doneTasksSource
+        .getValue()
+        .filter((task) => task !== pendingTask);
       const toEmitPending = this.pendingTasksSource.getValue();
       pendingTask.status = TaskStatus.Pending;
       toEmitPending.push(pendingTask);
       this.pendingTasksSource.next(toEmitPending);
-      this.doneTasksSource.next(toEmitDone);   
+      this.doneTasksSource.next(toEmitDone);
     }
   }
 
   deleteTask(id: number) {
-    const pending = this.pendingTasksSource.getValue().find(task => task.id === id);
+    const pending = this.pendingTasksSource
+      .getValue()
+      .find((task) => task.id === id);
     if (pending) {
-      const toEmitPending = this.pendingTasksSource.getValue().filter(task => task.id != id);
+      const toEmitPending = this.pendingTasksSource
+        .getValue()
+        .filter((task) => task.id != id);
       this.pendingTasksSource.next(toEmitPending);
     }
 
-    const done = this.doneTasksSource.getValue().find(task => task.id === id);
+    const done = this.doneTasksSource.getValue().find((task) => task.id === id);
     if (done) {
-      const toEmitDone = this.doneTasksSource.getValue().filter(task => task.id != id);
+      const toEmitDone = this.doneTasksSource
+        .getValue()
+        .filter((task) => task.id != id);
       this.pendingTasksSource.next(toEmitDone);
     }
   }
