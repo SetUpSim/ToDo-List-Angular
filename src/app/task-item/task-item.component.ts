08d5755c78a4488c7ffa257c7dfdefb531cc55be
Stasiuk.Illia <Stasiuk.Illia@scheidt-bachmann.de>
2022-08-25 16:03:03 +0300

    v2.0 Added delete task button

--- src/app/task-item/task-item.component.ts
+++ src/app/task-item/task-item.component.ts
@@ -1,23 +1,36 @@
 import { Component, Input } from '@angular/core';
-import { Task } from '../task.model'
+import { Task } from '../task.model';
+import { TasksContainerService } from '../tasks-container.service';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.css']
+  styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent {
-
   @Input() task: Task;
-  
-  constructor() {
-    this.task = {id:0, description: "Default task", dueDate: undefined, doneDate: undefined, status: 0};
+
+  constructor(public container: TasksContainerService) {
+    this.task = {
+      id: 0,
+      description: 'Default task',
+      dueDate: undefined,
+      doneDate: undefined,
+      status: 0,
+    };
+  }
+
+  onDeleteItem(event: Event) {
+    this.container.deleteTask(this.task.id);
   }
 
   dragStartHandler(event: DragEvent) {
     if (event.dataTransfer) {
-      event.dataTransfer.setData("text/plain", (event.target as HTMLElement).id);
-      event.dataTransfer.dropEffect = "move";
+      event.dataTransfer.setData(
+        'text/plain',
+        (event.target as HTMLElement).id
+      );
+      event.dataTransfer.dropEffect = 'move';
     }
   }
 }
