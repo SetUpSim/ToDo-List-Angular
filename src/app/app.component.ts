7251d424c648a13fa8be5f01ac74029d8d6fbdfa
Stasiuk.Illia <Stasiuk.Illia@scheidt-bachmann.de>
2022-09-07 12:52:14 +0300

    Rafactor app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { DateTimeService } from './datetime.service';
+import { TasksContainerService } from './tasks-container.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+  providers: [TasksContainerService, DateTimeService],
 })
 export class AppComponent {
   title = 'todo-list-app';
